f2bab9eb21c6171a1cbea35b9f549b8a7c384a33
Nicki Anselmo <nickianselmo@gmail.com>
2015-03-11 19:23:27 -0500

    adding footer and stamp gem to format time

--- rails/HomeSplash/app/controllers/home_page_controller.rb
+++ rails/HomeSplash/app/controllers/home_page_controller.rb
@@ -1,6 +1,8 @@
 class HomePageController < ApplicationController
   def index
-    @current_time = Time.now.to_formatted_s(:short)
+    time = Time.now
+    #uses the stamp method to help format it to match the string "3:00AM"
+    @current_time = time.stamp("3:00 AM")
     @greet_message = case Time.now.hour
                       when 5..11
                         "Good Morning!"
@@ -10,7 +12,9 @@ class HomePageController < ApplicationController
                         ["Good Evening", "Working Hard!"].sample
                       when 20..23, 0..4
                          "Goodnight"
-                      end     
+                      end    
+    date = Date.current 
+    @current_date = date.stamp("Sun Aug 16th")
                       
   end
 end
